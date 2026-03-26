5da5e9b50141be2c844f51deb22c93df6c60deed
Juan <juanchumbes08@gmail.com>
2026-03-25 20:30:10 -0500

    conectando a mysql

--- src/main/java/com/mitienda/tienda_api/model/Producto.java
+++ src/main/java/com/mitienda/tienda_api/model/Producto.java
@@ -1,8 +1,15 @@
 
 package com.mitienda.tienda_api.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-
+@Entity
 public class Producto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     private double precio;
